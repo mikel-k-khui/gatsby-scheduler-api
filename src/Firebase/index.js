ebc9678095b81194ef7b8b686329be350e540523
Mike Chui <mikel.k.khui@gmail.com>
2020-06-22 21:14:48 -0600

    Setup firebase; controller and logger

--- src/Firebase/index.js
+++
@@ -1,6 +0,0 @@
-import getFirebaseInstance from './firebase'
-import useAuth from './useAuth'
-import FirebaseContext from './context'
-
-export { FirebaseContext, useAuth }
-export default getFirebaseInstance
--- src/__test__/00setup.test.ts
+++
@@ -1,7 +0,0 @@
-const sum = require('../index')
-// setup testing
-describe('setup', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  })
-})
